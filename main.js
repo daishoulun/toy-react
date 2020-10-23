a1fb3c943100fe3214a4fc090cfbdc0d9f2d0980
daishoulun <970109789@qq.com>
2020-10-23 21:43:01 +0800

    20201023 commit

--- main.js
+++ main.js
@@ -1,19 +1,157 @@
 import { createElement, Component, render } from './toyReact'
 
-class MyToyReact extends Component {
+class Square extends Component {
   render() {
     return (
-      <div class="a">
-        <h1>my toy react</h1>
-        <ul>
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
-        </ul>
-        {this.children}
+      <button className="square" onClick={this.props.onClick}>
+        {this.props.value}
+      </button>
+    )
+  }
+}
+
+class Board extends Component {
+  renderSquare(i) {
+    return (
+      <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)}></Square>
+    )
+  }
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
       </div>
     )
   }
 }
 
-render(<MyToyReact id="toy" class="react"><h2>my toy react</h2></MyToyReact>, document.body)
+class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null)
+        }
+      ],
+      stepNumber: 0,
+      xIsNext: true
+    }
+  }
+
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1)
+    const current = history[history.length - 1]
+    const squares = current.squares.slice()
+    if (calculate(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O'
+    this.setState({
+      history: history.concat([
+        {
+          squares
+        }
+      ]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext
+    })
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
+    })
+  }
+
+  render() {
+    const history = this.state.history
+    const current = history[this.state.stepNumber]
+    const calculate = calculate(current.squares)
+
+    const moves = history.map((step, move) => {
+      const desc = move ? 'Go to move #' + move : 'Go to game start'
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      )
+    })
+
+    let status;
+    if (calculate) {
+      status = 'calculate' + calculate;
+    } else {
+      status = 'new Player' + (this.state.xIsNext ? 'X' : 'O')
+    }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board squares={current.squares} onClick={i => this.handleClick(i)}></Board>
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <div>{moves}</div>
+        </div>
+      </div>
+    )
+  }
+}
+
+render(<Game></Game>, document.getElementById('root'))
+
+function calculate(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a,b,c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+
+  return null
+}
+
+// class MyToyReact extends Component {
+//   render() {
+//     return (
+//       <div class="a">
+//         <h1>my toy react</h1>
+//         <ul>
+//           <li>1</li>
+//           <li>2</li>
+//           <li>3</li>
+//         </ul>
+//         {this.children}
+//       </div>
+//     )
+//   }
+// }
+
+// render(<MyToyReact id="toy" class="react"><h2>my toy react</h2></MyToyReact>, document.body)
--- toyReact.js
+++ toyReact.js
@@ -1,15 +1,32 @@
+const RENDER_TO_DOM = Symbol('render to dom')
+
 class ElementWrapper  {
   constructor(type) {
     this.root = document.createElement(type)
     console.log('elementWrapper')
   }
   setAttribute(name, value) {
+    if (name.match(/^on([\s\S]+)$/)) {
+      this.root.addEventListener(RegExp.$1.replace(/^[\s\S]/, c => c.toLowerCase()), value)
+    } else {
+      if (name === 'className') {
+        this.root.setAttribute('class', value)
+      } else {
+        this.root.setAttribute(name, value)
+      }
+    }
     console.log('elementWrapper---setAttribute')
-    this.root.setAttribute(name, value)
   }
   appendChild(component) {
     console.log('elementWrapper---appendChild')
-    this.root.appendChild(component.root)
+    let range = document.createRange()
+    range.setStart(this.root, this.root.childNodes.length)
+    range.setEnd(this.root, this.root.childNodes.length)
+    component[RENDER_TO_DOM](range)
+  }
+  [RENDER_TO_DOM](range) {
+    range.deleteContents();
+    range.insertNode(this.root)
   }
 }
 
@@ -18,14 +35,19 @@ class TextWrapper {
     console.log('TextWrapper')
     this.root = document.createTextNode(content)
   }
+  [RENDER_TO_DOM](range) {
+    range.deleteContents();
+    range.insertNode(this.root)
+  }
 }
 
 export class Component {
   constructor() {
     console.log('Component')
     this.props = Object.create(null);
-    this.children = []
+    this.children = []                      
     this._root = null;
+    this._range = null
   }
   setAttribute(name, value) {
     console.log('Component---setAttribute')
@@ -35,15 +57,48 @@ export class Component {
     console.log('Component---appendChild')
     this.children.push(component)
   }
-  get root() {
-    console.log('Component---get root')
-    console.log('Component---get root', this._root)
-    if (!this._root) {
-      this._root = this.render().root
-    }
-    console.log('Component---get root', this._root)
-    return this._root
+  [RENDER_TO_DOM](range) {
+    this._range = range
+    this.render()[RENDER_TO_DOM](range)
   }
+  rerender() {
+    let oldRange = this._range
+    let range = document.createRange()
+    range.setStart(oldRange.startContainer, oldRange.startOffset)
+    range.setEnd(oldRange.startContainer, oldRange.startOffset)
+    this[RENDER_TO_DOM](range)
+
+    oldRange.setStart(range.endContainer, range.endOffset)
+    oldRange.deleteContents()
+  }
+  setState(newState) {
+    if (this.state === null || typeof this.state !== 'object') {
+      this.state = newState
+      this.rerender()
+      return
+    }
+    let merge = (oldState, newState) => {
+      for (p in newState) {
+        if (oldState[p] === null || typeof oldState[p] !== 'object') {
+          oldState[p] = newState[p]
+        } else {
+          merge(oldState[p], newState[p])
+        }
+      }
+    }
+
+    merge(this.state, newState)
+    this.rerender()
+  }
+  // get root() {
+  //   console.log('Component---get root')
+  //   console.log('Component---get root', this._root)
+  //   if (!this._root) {
+  //     this._root = this.render().root
+  //   }
+  //   console.log('Component---get root', this._root)
+  //   return this._root
+  // }
 }
 
 export function createElement(type, attributes, ...children) {
@@ -64,6 +119,9 @@ export function createElement(type, attributes, ...children) {
       if (typeof child === 'string') {
         child = new TextWrapper(child);
       }
+      if (child === null) {
+        continue
+      }
       if ((typeof child === 'object') && (child instanceof Array)) {
         insertChildren(child)
       } else {
@@ -76,7 +134,12 @@ export function createElement(type, attributes, ...children) {
 }
 export function render(component, parentElement) {
   console.log('render')
-  parentElement.appendChild(component.root)
+  let range = document.createRange()
+  range.setStart(parentElement, 0)
+  range.setEnd(parentElement, parentElement.childNodes.length)
+  range.deleteContents()
+  component[RENDER_TO_DOM](range)
+  // parentElement.appendChild(component.root)
 }
 
 
